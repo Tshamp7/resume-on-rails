da49be989d8ccaad797efa95a8c23be387874582
Tom Shamp <T.shamp7@gmail.com>
2020-06-14 17:40:38 -0700

    Add about text for init heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    def about
+        "I am a programmer living near the Columbia river in Washougal, WA,
+        Just outside of Portland, OR. I decided to make a career change last year 
+        from pharmaceutical research to software engineering. My current interests
+        are Ruby, Rails, PostgresQL, RDBMS's, and I have recently begun learning React. 
+        I know Javascript as well, but am much more fluent in Ruby than JS. I am 
+        planning to focus more on Javascript in the coming month to better round out
+        my skillset."
+    end
 end
